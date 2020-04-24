76916722344548afa2720c0bfeb451b63f2e5b71
wbadmin <>
2020-04-24 10:58:34 +0530

    Master i2 {/src/main/java/com/DataMaster.java}

--- src/main/java/com/DataMaster.java
+++ src/main/java/com/DataMaster.java
@@ -10,6 +10,8 @@ public class DataMaster implements java.io.Serializable {
 
 	private java.lang.String i1;
 
+	private java.lang.Short i2;
+
 	public DataMaster() {
 	}
 
@@ -21,8 +23,17 @@ public class DataMaster implements java.io.Serializable {
 		this.i1 = i1;
 	}
 
-	public DataMaster(java.lang.String i1) {
+	public java.lang.Short getI2() {
+		return this.i2;
+	}
+
+	public void setI2(java.lang.Short i2) {
+		this.i2 = i2;
+	}
+
+	public DataMaster(java.lang.String i1, java.lang.Short i2) {
 		this.i1 = i1;
+		this.i2 = i2;
 	}
 
 }
