f22a0f2d350f65bfbcdaec34085a0d2fa85ea231
wbadmin <>
2020-04-24 11:25:53 +0530

    test10 i2 {/src/main/java/com/DataTest9.java}

--- src/main/java/com/DataTest9.java
+++ src/main/java/com/DataTest9.java
@@ -10,6 +10,8 @@ public class DataTest9 implements java.io.Serializable {
 
 	private java.lang.String i1;
 
+	private java.lang.String i2;
+
 	public DataTest9() {
 	}
 
@@ -21,8 +23,17 @@ public class DataTest9 implements java.io.Serializable {
 		this.i1 = i1;
 	}
 
-	public DataTest9(java.lang.String i1) {
+	public java.lang.String getI2() {
+		return this.i2;
+	}
+
+	public void setI2(java.lang.String i2) {
+		this.i2 = i2;
+	}
+
+	public DataTest9(java.lang.String i1, java.lang.String i2) {
 		this.i1 = i1;
+		this.i2 = i2;
 	}
 
 }
