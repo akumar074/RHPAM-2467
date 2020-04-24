e987113f14ddfb0ef11ef1b000f91af6856ce6e5
wbadmin <>
2020-04-24 11:01:29 +0530

    test2 i1 {/src/main/java/com/DataTest2.java}

--- src/main/java/com/DataTest2.java
+++ src/main/java/com/DataTest2.java
@@ -6,12 +6,23 @@ package com;
 
 public class DataTest2 implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public DataTest2() {
-    }
+	private java.lang.String i1;
 
+	public DataTest2() {
+	}
 
+	public java.lang.String getI1() {
+		return this.i1;
+	}
 
+	public void setI1(java.lang.String i1) {
+		this.i1 = i1;
+	}
+
+	public DataTest2(java.lang.String i1) {
+		this.i1 = i1;
+	}
 
 }
