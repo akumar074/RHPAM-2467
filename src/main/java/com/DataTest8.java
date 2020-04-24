960be22ac21699818cd3c28f7ca3b3d8cd5d4b14
wbadmin <>
2020-04-24 11:24:33 +0530

    test 8 i1 {/src/main/java/com/DataTest8.java}

--- src/main/java/com/DataTest8.java
+++ src/main/java/com/DataTest8.java
@@ -6,12 +6,23 @@ package com;
 
 public class DataTest8 implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public DataTest8() {
-    }
+	private java.lang.String i1;
 
+	public DataTest8() {
+	}
 
+	public java.lang.String getI1() {
+		return this.i1;
+	}
 
+	public void setI1(java.lang.String i1) {
+		this.i1 = i1;
+	}
+
+	public DataTest8(java.lang.String i1) {
+		this.i1 = i1;
+	}
 
 }
