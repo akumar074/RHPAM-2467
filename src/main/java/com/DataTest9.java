d969f0f51a5097ed92f72e1ddabe37079fbbea74
wbadmin <>
2020-04-24 11:25:18 +0530

    test9 {/src/main/java/com/DataTest9.java}

--- src/main/java/com/DataTest9.java
+++ src/main/java/com/DataTest9.java
@@ -6,12 +6,23 @@ package com;
 
 public class DataTest9 implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public DataTest9() {
-    }
+	private java.lang.String i1;
 
+	public DataTest9() {
+	}
 
+	public java.lang.String getI1() {
+		return this.i1;
+	}
 
+	public void setI1(java.lang.String i1) {
+		this.i1 = i1;
+	}
+
+	public DataTest9(java.lang.String i1) {
+		this.i1 = i1;
+	}
 
 }
